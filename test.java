06c1878c63c941c33e6f9222137e133566d34a2b
xiriying <xiriying520@163.com>
2026-03-26 18:47:06 +0800

    Add files via upload
    Lasted Update with 3/36/2026

---
+++ test.java
@@ -0,0 +1,12 @@
+public class test{
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        Character character = new Character();
+    }
+}
+
+class Character {
+    enum Girls{Akane, Ayano, Sakura}
+    Girls girls;
+    
+}
